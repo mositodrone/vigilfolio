b4272a91511fa5600671fd29f0660cafc4e64be1
mositodrone <mositodrone@gmail.com>
2020-07-12 15:20:23 +0100

    adding files

---
+++ src/yourdata.js
@@ -0,0 +1,83 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        name :'momodev.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! My name is Moses.',
+                        //Line 2 For Header
+                        'Im a Web Developer ',
+                        //Line 3 For Header
+                        'Welcome to my portfolio'
+    ],
+        //Contact Email
+        contactEmail:'mositodrone@gmail.com',
+        // Add Your About Text Here
+        abouttext: "About Me First of all i want to say....Thank you for your time spent checking my portfolio!! My name is moses ajayi and im a web-developer, as you've probably guessed. and don't mind me holding back on the flashy animations a bit :). i specialize in both frontend and server-side + database programming, its what i LOVE and i realize that if you are here looking to hire, you must have gone through other portfolio's like this one, and id like to say that, although im not the best developer out there, YOU have probably noticed that there are a shortage of developers , who are enthusiastic, egoless and ready to learn new skills with an open mind developers who dont know the true ethics, of being a develper, as you go through the projects below. id like to tell you that i've put a whole lot off effort into building it, and i hope you are impressed enough to hire me, YES, HIRE ME!!!, IM pretty sure that you've been in my shoes before, and id like to assure you, that in any project i take up or Manage, anything i create, will be my absolute best. you wont have to micro-manage me or put out any fires, and ill eventually become one of the best developers to have around. Thank you for reading this and please Enjoy viewing the projects below",
+        aboutImage:'https://images.unsplash.com/photo-1487147264018-f937fba0c817?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:true,//true or false (Change Here)
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'Smart-brain', //Project Title - Add Your Project Title Here
+             service:'facerecognition for humans', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             //Project URL - Add Your Project Url Here
+             url:'https://s-brain-app.herokuapp.com'
+            }
+           //  {
+           //      id: 2,//DO NOT CHANGE THIS (Please)😅
+           //      title: 'Project Two',
+           //      service: 'Website Development',
+           //      imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+           //      url: 'http://chetanverma.com/'
+           //  },
+           //  { 
+           //      id: 3,//DO NOT CHANGE THIS (Please)😅
+           //      title: 'Project Three',
+           //      service: 'Web App',
+           //      imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+           //      url: 'http://chetanverma.com/'
+           //  },
+           //  {
+           //      id: 4,//DO NOT CHANGE THIS (Please)😅
+           //      title: 'Project Four',
+           //      service: 'Branding',
+           //      imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+           //      url: 'http://chetanverma.com/'
+           // }
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/mositodrone'},
+            {
+                name: 'Behance',
+                url: 'https://www.behance.net/chetanverma'
+            },
+            {
+                name: 'Dribbble',
+                url: 'https://dribbble.com/chetanverma'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/cv.uidesign/'
+            }
+
+        ]
+    }
